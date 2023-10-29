334cf3e0be26fad79b175bc57bd3f5dc65509509
tataosmonalieva <emusya1010@mail.ru>
2023-10-29 18:27:47 +0600

    js

---
+++ script.js
@@ -0,0 +1,144 @@
+//user admin true welcome 'user'
+//false not correct user
+//function myFunction(user) {
+    //if (user === 'admin') {
+     // return 'Welcome user'
+    //} else {
+         //  return `not correct ${user}`
+      // }
+ //}
+//console.log(myFunction('admin'))
+//console.log(myFunction('user'))
+
+// today is 'day' of february
+// on february only 28 day, your day is 'day' of another month
+//function myFunction(day) {
+    //if(day >=1 && day <=28 ) {
+      //  return `today is ${day} of february`
+    //} else {
+      //  return `on february only 28 day, yuor day is ${day} of another month`
+   // }
+//}
+//console.log(myFunction(15))
+//console.log(myFunction(29))
+
+// 'num' is equals of 50
+// 'num' is not correct
+//function myFunction(num) {
+   // if(num == 50) {
+     //   return `${num} is equals of 50`
+    //} else {
+     //   return `${num} is not correct`
+    //}
+//}
+//console.log(myFunction(50))
+//console.log(myFunction(13))
+
+// 'user' 'age' old, welcome
+// 'user' 'age' old, rejected
+//function myFunction(age,user) {
+    //if (age >=18) {
+
+   // return `welcome ${user}  old `
+    
+//} else  {
+    //return `rejected ${user} old`
+//}
+//}
+//console.log(myFunction(35,'user'))
+//console.log(myFunction(17, 'user'))
+
+// 'user' name is too short (min 3)
+// 'user' name is accepted
+
+//function myFunction(user) {
+    //if(user.length<=3) {
+        //return `${user} name is too short`
+   // } else {
+       // return `${user} name is accepted`
+    //}
+//}
+//console.log(myFunction('sam'))
+//console.log(myFunction('ailin'))
+
+// 'user' name is too long (max 5)
+// 'user' name is accepted
+//function myFunction(user) {
+    //if(user.length>=5) {
+       // return `${user} name is too long`
+    //} else {
+       // return `${user} name is accepted`
+    //}
+//}
+//console.log(myFunction('emirlan'))
+//console.log(myFunction('emir'))
+
+// 'age' must be a number ('20')
+//function myFunction(age) {
+   // if(age  === '20') { 
+
+    //} else {
+      //  return `number ${age}`
+   // }
+//}
+//console.log(myFunction(20))
+
+
+// 'age' must be a string (20)
+// 'age' must be older then 23
+//function myFunction(age,string) {
+   // console.log(string)
+    // if(age <=23 && string === '20') { 
+        //return `must be older then ${age}`
+ 
+     //} else  {
+      //  return `be a ${string}`
+     //}
+ //}
+    // console.log(myFunction(23,'20'))
+
+    // today is monday/tuesday/wednesday/thursday/friday weekend
+// max days of weeks 7
+//function myFunction(days) {
+    //if(days === 1) {
+       // return `monday`
+    //} else if(days === 2) {
+       // return `tuesday`
+    //} else if(days === 3) {
+        //return `wednesday`
+    //} else if(days === 4) {
+      //  return `thursday`
+    //} else if(days === 5) {
+       // return `friday`
+    //} else if (days === 6 || days === 7) {
+       // return `weekend`
+
+    //} else if(days >=8) {
+       // return `max days of weeks 7`
+   // }
+//}
+//console.log(myFunction(1))
+//console.log(myFunction(7))
+//console.log(myFunction(8))
+    
+// 1. проверить длинну на четность
+
+//function myFunction(a) {
+    //if(a.length%2 === 0) {
+       // return`длина ${a} четная`
+    //} else {
+    //return `длина ${a} нечетная`
+    //}
+//}
+//console.log(myFunction('kunduz'))
+//console.log(myFunction('tanya'))
+
+// 1. lorem ipsum dolor sit amet => loremipsumdolorsitamet
+// price и discount
+//function myFunction(str,a) {
+    //if(str.includes(" ")) {
+       // a = str.replaceAll(' ','')
+        //return a
+    //}
+//}
+ //console.log(myFunction('lorem ipsum dolor sit amet'))
